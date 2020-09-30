9afefa90cf58d17f6736140ae43f1d8bd94bfcdc
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-09-30 19:40:33 +0200

    First regular expression $ - at the end of the strign

--- RegularExpression.py
+++ RegularExpression.py
@@ -2,3 +2,22 @@
 Regular expressions are a powerful and (mostly) standardized way of 
 searching, replacing, and parsing text with complex patterns of characters.
 '''
+
+'''
+"$" - at the end od the string
+'''
+
+import re
+
+
+# Street Adresses
+
+street_adress = '100 NORTH BROAD ROAD'
+street_adress = street_adress.replace('ROAD', 'RD.')
+
+print(street_adress)
+
+street_adress = '100 NORTH BROAD ROAD'
+street_adress = re.sub('ROAD$', 'RD.', street_adress)
+print(street_adress)
+
