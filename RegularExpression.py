a97a52a9b10e9da4bf646345ab809f970ea79201
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-09-30 20:09:10 +0200

    r... - Raw strings, ...\b... - as a boundary

--- RegularExpression.py
+++ RegularExpression.py
@@ -1,10 +1,15 @@
 '''
 Regular expressions are a powerful and (mostly) standardized way of 
 searching, replacing, and parsing text with complex patterns of characters.
+PROTIP:
+I recommend always using raw strings when dealing with regular expressions
 '''
 
 '''
-"$" - at the end od the string
+"...$" - at the end od the string
+"^..." - at the beginning of the string
+"\b..." - a word boundary must occur here
+r"..." - makes string "RAW" so nothing in this string should be escaped
 '''
 
 import re
@@ -20,4 +25,14 @@ print(street_adress)
 street_adress = '100 NORTH BROAD ROAD'
 street_adress = re.sub('ROAD$', 'RD.', street_adress)
 print(street_adress)
+
+street_adresses = ['100 BROAD','100 BROAD ROAD APT. 3']
+fixed_street_adresses = []
+for name in street_adresses:
+    fixed_street_adresses.append(re.sub(r'\bROAD\b', 'RD.', name))
+
+print(fixed_street_adresses)
+
+
+
 
